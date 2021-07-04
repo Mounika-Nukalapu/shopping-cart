ff10d8b0f02806bdea6ec33ff585622579452eb9
Mounika-Nukalapu <mounika.nukalapu2001@gmail.com>
2021-07-04 15:36:05 +0530

    Initial commit

---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,117 @@
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items: [
+        {id:1,title:'ZENEME', desc: "Zeneme Jewellery Set American Diamond Stylish Peacock Style Gold Plated Necklace Set Jewellery With Earring For Women & Girls", price:600,img:Item1},
+        {id:2,title:'Touchstone', desc: "Touchstone Indian Bollywood Rhinestones Bridal Jewelry Necklace Set for Women", price:900,img: Item2},
+        {id:3,title:'Shining Diva Fashion', desc:"Shining Diva Fashion Crystal Party Wear Pearl Necklace Set For Women / Jewellery Set with Earrings for Women & Girls(White)(rrsd8494s)",price:600,img: Item3},
+        {id:4,title:'Ratnavali Jewels', desc: "Ratnavali Jewels American Diamond CZ Rose Gold Plated Designer Jewellery Set/Necklace Set with Chain & Earring for Girls/Women (RV3382RG)", price:1700,img:Item4},
+        {id:5,title:'Ratnavali Jewels', desc: "Ratnavali Jewels American Diamond Traditional Fashion Jewellery Red Necklace Pendant Set with Earring for Women/Girls RV3203R", price:1600,img: Item5},
+        {id:6,title:'YouBella', desc: "YouBella Jewellery Set for Women American Diamond Combo of Necklace Set with Earrings, Bracelet and Ring for Girls and Women",price:499,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+
+export default cartReducer
